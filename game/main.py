ab2a4718726981fb1cf0b8be2f95ede6814449e4
Chuck MacKinnon <chuckwm@gmail.com>
2018-01-27 20:07:32 -0500

    adding game dir

---
+++ game/main.py
@@ -0,0 +1,35 @@
+from player import Player
+
+tim = Player("Tim")
+
+print(tim.name)
+print(tim.lives)
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
+
+tim._lives = 9
+print(tim)
+
+tim.level = 2
+print(tim)
+
+tim.level += 5
+print(tim)
+
+tim.level = 3
+print(tim)
+
+tim.level = 1
+print(tim)
+
+tim.score = 500
+print(tim)
