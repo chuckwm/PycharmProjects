88cb89f39aba91caf83ab7637d4ca1d536371013
Chuck MacKinnon <chuckwm@gmail.com>
2018-01-28 14:27:55 -0500

    Adding changes for oop/game

--- game/main.py
+++ game/main.py
@@ -1,35 +1,10 @@
-from player import Player
+from enemy import Enemy, Troll
 
-tim = Player("Tim")
+ugly_troll = Troll()
+print("Ugly troll - {}".format(ugly_troll))
 
-print(tim.name)
-print(tim.lives)
-tim.lives -= 1
-print(tim)
+another_troll = Troll("Ug", 18, 3)
+print("Another troll - {}".format(another_troll))
 
-tim.lives -= 1
-print(tim)
-
-tim.lives -= 1
-print(tim)
-
-tim.lives -= 1
-print(tim)
-
-tim._lives = 9
-print(tim)
-
-tim.level = 2
-print(tim)
-
-tim.level += 5
-print(tim)
-
-tim.level = 3
-print(tim)
-
-tim.level = 1
-print(tim)
-
-tim.score = 500
-print(tim)
+brother = Troll("Urg", 23)
+print(brother)
